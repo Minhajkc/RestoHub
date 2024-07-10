ea9b16fd029ccefdf70d3fd489bf2c277d5b4579
minhajkc <minhajkc00@gmail.com>
2024-07-10 12:15:41 +0530

    vercel vite error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
+import Header from 'components/Header';
 import WaiterPage from './pages/WaiterPage';
 import ChefPage from './pages/ChefPage';
 import RecentOrders from './components/RecentOrders';
